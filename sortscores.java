038dfdf34fd67bbbad1ba504f3f9f4e5cd0afc7c
Nakeeb <mmohammadnakeeb@gmail.com>
2026-02-23 22:10:18 +0530

    Refactor sorting algorithm for clarity and correctness

--- sortscores.java
+++ sortscores.java
@@ -1,23 +1,37 @@
 import java.util.*;
-class Main{
+
+class Main {
   public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    int arr[]=new int[n];
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter number of elements: ");
+    int n = sc.nextInt();
+
+    int arr[] = new int[n];
+
+    System.out.println("Enter " + n + " numbers:");
+
     for(int i=0;i<n;i++){
-      arr[i]=sc.nextInt();
+      arr[i] = sc.nextInt();
     }
+
     for(int i=0;i<n-1;i++){
-      int min_idx=i;
+      int min = i;
+
       for(int j=i+1;j<n;j++){
-        if(arr[j]<arr[min_idx])
-          min_idx=j;
+        if(arr[j] < arr[min])
+          min = j;
       }
-      int temp=arr[i];
-      arr[i]=arr[min_idx];
-      arr[min_idx]=temp;
+
+      int temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
     }
+
+    System.out.println("Sorted numbers:");
+
     for(int i=0;i<n;i++)
-      System.out.print(arr[i]+" ");
+      System.out.print(arr[i] + " ");
   }
 }
