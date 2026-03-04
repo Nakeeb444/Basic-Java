147360fa15a492d465a3d434e8bb5bf8bd1ac41f
Nakeeb <mmohammadnakeeb@gmail.com>
2026-03-04 23:27:48 +0530

    Remove main method from BankDemo class
    Removed main method and related user interaction code.

--- BankDemo.java
+++ BankDemo.java
@@ -23,25 +23,3 @@ class BankAccount {
         }
     }
 }
-
-public class BankDemo {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BankAccount account = new BankAccount("User", 1000.0);
-        String command = "";
-
-        System.out.println("Commands: deposit, withdraw, exit");
-
-        while (!command.equals("exit")) {
-            System.out.print("Enter command: ");
-            command = sc.next();
-
-            if (command.equals("deposit")) {
-                account.deposit(sc.nextDouble());
-            } else if (command.equals("withdraw")) {
-                account.withdraw(sc.nextDouble());
-            }
-        }
-        sc.close();
-    }
-}
