2a9b64844d603fb8994ee8b527ca27a6d8f759c4
Nakeeb <mmohammadnakeeb@gmail.com>
2026-03-10 14:59:15 +0530

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class PrimeNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Prime numbers up to " + n + ":");
+
+        for(int i = 2; i <= n; i++) {
+
+            int count = 0;
+
+            for(int j = 1; j <= i; j++) {
+                if(i % j == 0) {
+                    count++;
+                }
+            }
+
+            if(count == 2) {
+                System.out.println(i);
+            }
+        }
+    }
+}
